8b8c14c9de05d52b3e42f2620635daea1bd355ad
Myrto Koukouli <myrtokoukouli@gmail.com>
2026-04-24 13:03:29 +0200

    fixed the ORCID regex in two more places

--- ckanext/cuprit/lib/clean_and_extract_orcid_rorid.py
+++ ckanext/cuprit/lib/clean_and_extract_orcid_rorid.py
@@ -4,9 +4,9 @@ def clean_and_extract_orcid(name_string):
     """
     Cleans the name string by removing the ORCID identifiers and extracting ORCID if present.
     """
-    orcid_match = re.search(r"\((\d{4}-\d{4}-\d{4}-\d{4})\)", name_string)
+    orcid_match = re.search(r"\((\d{4}-\d{4}-\d{4}-\d{3}[\dX])\)", name_string)
     orcid = orcid_match.group(1) if orcid_match else None
-    cleaned_name = re.sub(r"\(\d{4}-\d{4}-\d{4}-\d{4}\)", "", name_string).strip()
+    cleaned_name = re.sub(r"\(\d{4}-\d{4}-\d{4}-\d{3}[\dX]\)", "", name_string).strip()
     return cleaned_name, orcid
 
 def clean_and_extract_rorid(name_string):
